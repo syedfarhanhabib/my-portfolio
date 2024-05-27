b7f233dfeedc6de6c510298cbf04565cc192e457
syedfarhanhabib <syedfarhandesigns@gmail.com>
2024-05-27 03:45:03 -0700

    card anim

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -10,11 +10,11 @@ function page() {
     <section className='min-h-dvh bg-light text-dark dark:bg-dark dark:text-light px-5 md:px-10 pt-20 md:pt-40 pb-10 '>
       <h3 className='tracking-tighter flex items-center text-sm md:text-lg py-1 px-2 leading-none w-fit rounded-full mb-3 dark:text-dark dark:bg-light bg-dark text-light'>Case studies</h3>
       <Text className=''>Crafting intuitive interfaces that bring dreams to digital reality.</Text>
-      <div className="cards flex flex-col md:flex-row gap-10 mt-10 ">
+      <div className="cards flex flex-col md:flex-row gap-5 md:gap-10 mt-10 ">
         <Card img='/euroauto.jpg' text='project 01' customClass='' link='/' />
         <Card img='/puntopago.jpg' text='project 02' customClass='md:mt-40' link='/' />
       </div>
-      <div className="cards flex flex-col md:flex-row gap-10 mt-10 ">
+      <div className="cards flex flex-col md:flex-row gap-5 md:gap-10 mt-10 ">
         <Card img='/riyadh.jpg' text='project 03' customClass='' link='/' />
         <Card img='/kelvin.jpg' text='project 04' customClass='md:mt-40' link='/' />
       </div>
--- src/components/WorkPage/Card.tsx
+++ src/components/WorkPage/Card.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { neueMachina } from '@/app/fonts';
+import { motion } from 'framer-motion';
 
 interface CardProps {
   customClass: string;
@@ -16,13 +17,22 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ customClass, text, img, link }) => {
   return (
     <>
-      <div className={`${customClass} relative w-full h-full`}>
-        <Image className='w-full h-full object-cover hover:scale-[1.009] shadow-xl rounded-3xl transition-all duration-300' height={500} width={500} src={img} alt='Project cover' />
-        <Link href={link}>
-        </Link>
-        <h1 className={`${neueMachina.className} tracking-tightest text-4xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-br from-dark dark:from-light dark:to-depth-dark to-depth-light leading-none mt-2`}>
-          {text}
-        </h1>
+      <div className={`${customClass} overflow-hidden`}>
+        <motion.div
+          initial={{ x: "-100%", }}
+          whileInView={{ x: "0%", }}
+          transition={{
+            duration: .4,
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+          }}
+          className={`relative w-full md:w-[40vw] md:h-[80dvh]`}>
+          <Image className=' relative w-full h-full object-cover hover:scale-[1.009] shadow-xl rounded-3xl transition-all duration-300' height={500} width={500} src={img} alt='Project cover' />
+          <Link style={{ backgroundColor: "#eab308" }} className={`${neueMachina.className} tracking-tightest text-4xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-br from-dark dark:from-light dark:to-depth-dark to-depth-light leading-none absolute left-5 bottom-5`} href={link}>
+            {text}
+          </Link>
+        </motion.div>
       </div>
     </>
   );
